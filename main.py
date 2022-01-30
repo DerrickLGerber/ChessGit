926b9e33d15fd3892cf757291261a186b0457cf3
DerrickLGerber <98016241+DerrickLGerber@users.noreply.github.com>
2022-01-30 11:29:15 +0200

    Remove print_hi

--- main.py
+++ main.py
@@ -7,14 +7,8 @@ import Chess.ChessMain
 from Chess import ChessMain
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    #print_hi('PyCharm')
     ChessMain.main()
 
 
